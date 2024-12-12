b8892bd6386479d41dfa35f33557b892acb8ac06
xbonda06 <mediabondar@gmail.com>
2024-12-12 22:16:36 +0100

    feat: add AppColors to colors.dart

---
+++ lib/theme/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF576421);
+  static const Color surface = Color(0XFFE1E6C3);
+  static const Color accent = Color(0xFFFFFFFF);
+  static const Color success = Color(0xFF4CAF50);
+  static const Color error = Color(0xFFBA1A1A);
+  static const Color background = Color(0xFFFBFAED);
+  static const Color textPrimary = Color(0xFF1B1C15);
+  static const Color textSecondary = Color(0xFF77786A);
+}
